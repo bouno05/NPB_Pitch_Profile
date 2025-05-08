011bf3a1ecf406f85e72c141bae2a3234ee7c190
bouno05 <bouno05@gmail.com>
2025-05-08 22:59:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ st.markdown("【 [NPB Bat Profiler](https://npbbatprofile-7knoehzqmixokxxxj2weeq
 
 # 年度を選択
 selected_year = st.selectbox("Select Year", list(data.keys()))
-
+st.markdown("※2025年は5/7終了時点のデータ")
 # 選択した年度のリーグを取得
 if selected_year:
     leagues = data[selected_year]
     selected_league = st.selectbox("Select League", list(leagues.keys()))
-　　st.markdown("※2025年は5/7終了時点のデータ")
+　　
     # 選択したリーグの球団を取得
     if selected_league:
         teams = leagues[selected_league]
